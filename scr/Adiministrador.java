e143829c26b5fee14c02560bec831038a0c189ea
Koehler <bernardoskoehler@gmail.com>
2026-06-26 21:55:02 -0300

    Create Adiministrador class extending Conta

---
+++ scr/Adiministrador.java
@@ -0,0 +1,13 @@
+public class Adiministrador extends Conta{
+    Adiministrador(String nome, String email, String senha) {
+        super(nome, email, senha);
+    }
+
+    public void adicionarLivro() {
+
+    }
+
+    public void removerLivro(int idLivro) {
+
+    }
+}
