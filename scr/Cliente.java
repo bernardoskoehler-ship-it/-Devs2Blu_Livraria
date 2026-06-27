ecf79096e4b66f460b9c0d7a998255f631c7a034
Koehler <bernardoskoehler@gmail.com>
2026-06-26 21:54:20 -0300

    Create Cliente.java

---
+++ scr/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente extends Conta {
+    Cliente(String nome, String email, String senha) {
+        super(nome, email, senha);
+    }
+
+    public void alugarLivro(int idLivro) {
+
+    }
+    public void devolverLivro(int idLivro) {
+
+    }
+}
