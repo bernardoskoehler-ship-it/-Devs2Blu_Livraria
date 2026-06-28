094a21b1b6bb6df0e818aef2210cc20ea409b36e
Koehler <bernardoskoehler@gmail.com>
2026-06-27 23:42:26 -0300

    Refactor Conta class constructor and setter methods
    Refactor Conta class to include id as a parameter and update setter methods to return boolean.

--- scr/Conta.java
+++ scr/Conta.java
@@ -1,39 +1,47 @@
+package org.example;
+
 abstract class Conta {
-    int id;
+    private int id;
     private String nome;
     private String email;
     private String senha;
     private boolean logado = false;
 
-    Conta(String nome, String email, String senha) {
+    Conta(int id, String nome, String email, String senha) {
+        this.id = id;
         setNome(nome);
         setEmail(email);
         setSenha(senha);
     }
 
-    // GET/SET //
-    private void setNome(String nome) {
+    private boolean setNome(String nome) {
         if(nome != null && !nome.trim().isEmpty()) {
             this.nome = nome;
+            return true;
         }
+        return false;
     }
     public String getNome() {
         return nome;
     }
 
-    private void setEmail(String email) {
+    private boolean setEmail(String email) {
         if(email != null && !email.trim().isEmpty()) {
             this.email = email;
+            return true;
         }
+        return false;
     }
     public String getEmail() {
         return email;
     }
 
-    private void setSenha(String senha) {
+    private boolean setSenha(String senha) {
         if( senha != null && !senha.trim().isEmpty() && senha.length() > 7) {
             this.senha = senha;
+            return true;
         }
+        return false;
     }
     public String getSenha() {
         return senha;
@@ -43,7 +51,7 @@ abstract class Conta {
         return logado;
     }
 
-
-
-
+    public int getId() {
+        return id;
+    }
 }
