7062c4970eca842a720e6b3aae4be80fa9228dfd
Koehler <bernardoskoehler@gmail.com>
2026-06-27 23:42:49 -0300

    Update Adiministrador.java

--- scr/Adiministrador.java
+++ scr/Adiministrador.java
@@ -1,13 +1,42 @@
-public class Adiministrador extends Conta{
-    Adiministrador(String nome, String email, String senha) {
-        super(nome, email, senha);
+package org.example;
+
+public class Administrador extends Conta{
+    Administrador(int id, String nome, String email, String senha) {
+        super(id, nome, email, senha);
     }
 
-    public void adicionarLivro() {
-
+    public boolean valorValido(int valor) {
+        if(valor > 0) {
+            return true;
+        }
+        return false;
+    }
+    public boolean nomeValido(String nome) {
+        if(nome != null && !nome.trim().isEmpty()) {
+            return true;
+        }
+        return false;
     }
 
-    public void removerLivro(int idLivro) {
+    public boolean adicionarLivro(Biblioteca biblioteca, String nome, String autor, int anoLancamento, String genero) {
+        if (!nomeValido(nome) || !nomeValido(autor) || !valorValido(anoLancamento)) {
+            return false;
+        }
+        int id = biblioteca.gerarProximoIdLivro();
+        Livro livro = new Livro(id, nome, autor, anoLancamento, genero);
+        biblioteca.adicionarLivro(livro);
+        return true;
+    }
 
+    public boolean removerLivro(Biblioteca biblioteca, int idLivro) {
+        Livro livro = biblioteca.buscarLivro(idLivro);
+        if(livro == null) {
+            return false;
+        }
+        if(!livro.isDisponivel()) {
+            return false;
+        }
+        biblioteca.removerLivro(livro);
+        return true;
     }
 }
