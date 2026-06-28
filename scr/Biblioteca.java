4e9743e328d24b6984833a30cf84b5765608df35
Koehler <bernardoskoehler@gmail.com>
2026-06-27 23:42:02 -0300

    Update Biblioteca.java

--- scr/Biblioteca.java
+++ scr/Biblioteca.java
@@ -1,8 +1,46 @@
+package org.example;
+
 import java.util.ArrayList;
 
 public class Biblioteca {
 
-    public void emprestarLivro(int idCliente, int idLivro) {
+    private ArrayList<Livro> livros = new ArrayList<>();
 
+    private ArrayList<Conta> clientes = new ArrayList<>();
+
+    public Livro buscarLivro(int idLivro) {
+        for (Livro livro : livros) {
+            if(livro.getIdLivro() == idLivro) {
+                return livro;
+            }
+        }
+        return null;
+    }
+
+    public int gerarProximoIdLivro() {
+        if (livros.isEmpty()) {
+            return 1;
+        }
+        return livros.get(livros.size() - 1).getIdLivro() + 1;
+    }
+
+    public int gerarProximoIdCliente() {
+        if (clientes.isEmpty()) {
+            return 1;
+        }
+        return clientes.get(clientes.size() - 1).getId() + 1;
+    }
+
+    public void cadastrarCliente(String nome, String email, String senha) {
+        Conta cliente = new Cliente(gerarProximoIdCliente(), nome, email, senha);
+        clientes.add(cliente);
+    }
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public boolean removerLivro(Livro livro) {
+        return livros.remove(livro);
     }
 }
