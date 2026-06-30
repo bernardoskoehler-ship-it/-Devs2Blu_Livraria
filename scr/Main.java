4eba8a0758d4aeb563de11c73f5eb0824c40907d
Koehler <bernardoskoehler@gmail.com>
2026-06-29 21:10:55 -0300

    Update Main.java

--- scr/Main.java
+++ scr/Main.java
@@ -1,2 +1,10 @@
+package org.example;
+
 public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        Biblioteca biblioteca = new Biblioteca();
+        Conta adm = new Administrador("ADM", "ADM@gmail.com", "12345678");
+        menu.menuBiblioteca(biblioteca);
+    }
 }
